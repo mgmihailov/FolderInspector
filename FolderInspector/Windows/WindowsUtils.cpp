b0048d7866db4166b35bd8a9b34e27dbd2848681
Mihail Mihaylov <mgmihailov74@gmail.com>
2020-07-16 22:39:26 +0300

    Add WindowsUtils.cpp
    - Migrate function definition to cpp file to avoid duplicated symbols
      when linking.
    
    - Make all utility global variables const.

---
+++ FolderInspector/Windows/WindowsUtils.cpp
@@ -0,0 +1,58 @@
+#include "WindowsUtils.h"
+
+void ParseAttributes(const Filters& filters, DWORD& outIncludeAttribs, DWORD& outExcludeAttribs)
+{
+	outIncludeAttribs = 0;
+	outExcludeAttribs = 0;
+
+	if ((filters.AreIncludeAttribsEmpty() && filters.AreExcludeAttribsEmpty()))
+	{
+		outIncludeAttribs = DefaultIncludeAttributes;
+		outExcludeAttribs = DefaultExcludeAttributes;
+	}
+	else
+	{
+		for (const auto& attr : filters.GetIncludeAttribs())
+		{
+			auto it = AttrArgsToFlags.find(attr);
+			if (it != AttrArgsToFlags.end())
+			{
+				outIncludeAttribs |= AttrArgsToFlags.at(attr);
+			}
+			else
+			{
+				std::cout << "Attribute \"" << attr << "\" to be included is either invalid or not supported for this platform!" << std::endl;
+			}
+		}
+
+		for (const auto& attr : filters.GetExcludeAttribs())
+		{
+			auto it = AttrArgsToFlags.find(attr);
+			if (it != AttrArgsToFlags.end())
+			{
+				outExcludeAttribs |= AttrArgsToFlags.at(attr);
+			}
+			else
+			{
+				std::cout << "Attribute \"" << attr << "\" to be excluded is either invalid or not supported for this platform!" << std::endl;
+			}
+		}
+
+		DWORD duplicateAttribsExcludeMask = outIncludeAttribs & outExcludeAttribs;
+		outIncludeAttribs &= ~outExcludeAttribs;
+		outExcludeAttribs &= ~duplicateAttribsExcludeMask;
+
+		if (outIncludeAttribs == 0)
+		{
+			if (outExcludeAttribs != 0)
+			{
+				outIncludeAttribs = AllFileAttributes & ~outExcludeAttribs;
+			}
+			else
+			{
+				outIncludeAttribs = DefaultIncludeAttributes;
+				outExcludeAttribs = DefaultExcludeAttributes;
+			}
+		}
+	}
+}
--- FolderInspector/Windows/WindowsUtils.h
+++ FolderInspector/Windows/WindowsUtils.h
@@ -6,91 +6,58 @@
 #include <unordered_map>
 #include <algorithm>
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <Windows.h>
 
-std::unordered_map<std::string, DWORD> ArgsToFlags = {
-	{"arch", FILE_ATTRIBUTE_ARCHIVE},
-	{"compr", FILE_ATTRIBUTE_COMPRESSED},
-	{"dir", FILE_ATTRIBUTE_DIRECTORY},
-	{"enc", FILE_ATTRIBUTE_ENCRYPTED},
-	{"hidden", FILE_ATTRIBUTE_HIDDEN},
-	{"intstr", FILE_ATTRIBUTE_INTEGRITY_STREAM},
-	{"reg", FILE_ATTRIBUTE_NORMAL},
-	{"notidx", FILE_ATTRIBUTE_NOT_CONTENT_INDEXED},
-	{"offln", FILE_ATTRIBUTE_OFFLINE},
-	{"uchg", FILE_ATTRIBUTE_READONLY},
-	{"distr", FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS},
-	{"notlcl", FILE_ATTRIBUTE_RECALL_ON_OPEN},
-	{"lnk", FILE_ATTRIBUTE_REPARSE_POINT},
-	{"sparse", FILE_ATTRIBUTE_SPARSE_FILE},
-	{"sys", FILE_ATTRIBUTE_SYSTEM},
-	{"temp", FILE_ATTRIBUTE_TEMPORARY}
+#include "../Utils.h"
+
+const std::string ATTR_ARCH = { "arch" };
+const std::string ATTR_COMPR = { "compr" };
+const std::string ATTR_DIR = { "dir" };
+const std::string ATTR_ENC = { "enc" };
+const std::string ATTR_HIDDEN = { "hidden" };
+const std::string ATTR_INTSTR = { "intstr" };
+const std::string ATTR_REG = { "reg" };
+const std::string ATTR_NOTIDX = { "notidx" };
+const std::string ATTR_OFFLN = { "offln" };
+const std::string ATTR_UCHG = { "uchg" };
+const std::string ATTR_DISTR = { "distr" };
+const std::string ATTR_NOTCTL = { "notlcl" };
+const std::string ATTR_LNK = { "lnk" };
+const std::string ATTR_SPARSE = { "sparse" };
+const std::string ATTR_SYS = { "sys" };
+const std::string ATTR_TEMP = { "temp" };
+
+const std::unordered_map<std::string, DWORD> AttrArgsToFlags = {
+	{ATTR_ARCH, FILE_ATTRIBUTE_ARCHIVE},
+	{ATTR_COMPR, FILE_ATTRIBUTE_COMPRESSED},
+	{ATTR_DIR, FILE_ATTRIBUTE_DIRECTORY},
+	{ATTR_ENC, FILE_ATTRIBUTE_ENCRYPTED},
+	{ATTR_HIDDEN, FILE_ATTRIBUTE_HIDDEN},
+	{ATTR_INTSTR, FILE_ATTRIBUTE_INTEGRITY_STREAM},
+	{ATTR_REG, FILE_ATTRIBUTE_NORMAL},
+	{ATTR_NOTIDX, FILE_ATTRIBUTE_NOT_CONTENT_INDEXED},
+	{ATTR_OFFLN, FILE_ATTRIBUTE_OFFLINE},
+	{ATTR_UCHG, FILE_ATTRIBUTE_READONLY},
+	{ATTR_DISTR, FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS},
+	{ATTR_NOTCTL, FILE_ATTRIBUTE_RECALL_ON_OPEN},
+	{ATTR_LNK, FILE_ATTRIBUTE_REPARSE_POINT},
+	{ATTR_SPARSE, FILE_ATTRIBUTE_SPARSE_FILE},
+	{ATTR_SYS, FILE_ATTRIBUTE_SYSTEM},
+	{ATTR_TEMP, FILE_ATTRIBUTE_TEMPORARY}
 };
 
-DWORD DefaultIncludeAttributes = ArgsToFlags["arch"] | ArgsToFlags["reg"]
-	| ArgsToFlags["notidx"] | ArgsToFlags["uchg"] | ArgsToFlags["dir"];
+const DWORD DefaultIncludeAttributes = AttrArgsToFlags.at(ATTR_ARCH) | AttrArgsToFlags.at(ATTR_REG)
+| AttrArgsToFlags.at(ATTR_NOTIDX) | AttrArgsToFlags.at(ATTR_UCHG) | AttrArgsToFlags.at(ATTR_DIR);
 
-DWORD DefaultExcludeAttributes = ArgsToFlags["compr"] | ArgsToFlags["enc"]
-	| ArgsToFlags["hidden"] | ArgsToFlags["intstr"]
-	| ArgsToFlags["offln"] | ArgsToFlags["distr"] | ArgsToFlags["notlcl"]
-	| ArgsToFlags["lnk"] | ArgsToFlags["sparse"] | ArgsToFlags["sys"]
-	| ArgsToFlags["temp"];
+const DWORD DefaultExcludeAttributes = AttrArgsToFlags.at(ATTR_COMPR) | AttrArgsToFlags.at(ATTR_ENC)
+| AttrArgsToFlags.at(ATTR_HIDDEN) | AttrArgsToFlags.at(ATTR_INTSTR)
+| AttrArgsToFlags.at(ATTR_OFFLN) | AttrArgsToFlags.at(ATTR_DISTR) | AttrArgsToFlags.at(ATTR_NOTCTL)
+| AttrArgsToFlags.at(ATTR_LNK) | AttrArgsToFlags.at(ATTR_SPARSE) | AttrArgsToFlags.at(ATTR_SYS)
+| AttrArgsToFlags.at(ATTR_TEMP);
 
-DWORD AllFileAttributes = DefaultIncludeAttributes | DefaultExcludeAttributes;
+const DWORD AllFileAttributes = DefaultIncludeAttributes | DefaultExcludeAttributes;
 
-void ParseAttributes(const Filters& filters, DWORD& outIncludeAttribs, DWORD& outExcludeAttribs)
-{
-	outIncludeAttribs = 0;
-	outExcludeAttribs = 0;
-
-	if ((filters.AreIncludeAttribsEmpty() && filters.AreExcludeAttribsEmpty()))
-	{
-		outIncludeAttribs = DefaultIncludeAttributes;
-		outExcludeAttribs = DefaultExcludeAttributes;
-	}
-	else
-	{
-		for (const auto& attr : filters.GetIncludeAttribs())
-		{
-			auto it = ArgsToFlags.find(attr);
-			if (it != ArgsToFlags.end())
-			{
-				outIncludeAttribs |= ArgsToFlags[attr];
-			}
-			else
-			{
-				std::cout << "Attribute \"" << attr << "\" to be included is either invalid or not supported for this platform!" << std::endl;
-			}
-		}
-
-		for (const auto& attr : filters.GetExcludeAttribs())
-		{
-			auto it = ArgsToFlags.find(attr);
-			if (it != ArgsToFlags.end())
-			{
-				outExcludeAttribs |= ArgsToFlags[attr];
-			}
-			else
-			{
-				std::cout << "Attribute \"" << attr << "\" to be excluded is either invalid or not supported for this platform!" << std::endl;
-			}
-		}
-
-		DWORD duplicateAttribsExcludeMask = outIncludeAttribs & outExcludeAttribs;
-		outIncludeAttribs &= ~outExcludeAttribs;
-		outExcludeAttribs &= ~duplicateAttribsExcludeMask;
-
-		if (outIncludeAttribs == 0)
-		{
-			if (outExcludeAttribs != 0)
-			{
-				outIncludeAttribs = AllFileAttributes & ~outExcludeAttribs;
-			}
-			else
-			{
-				outIncludeAttribs = DefaultIncludeAttributes;
-				outExcludeAttribs = DefaultExcludeAttributes;
-			}
-		}
-	}
-}
+void ParseAttributes(const Filters& filters, DWORD& outIncludeAttribs, DWORD& outExcludeAttribs);
